0787368078e8026658ed5bac18b45c5e6687edb7
himanshuchawla009 <himanshuchawla2014@gmail.com>
2019-11-10 23:15:27 +0530

    paging and pruchase requests api added

--- models/purchaseRequests.js
+++ models/purchaseRequests.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const PurchaseRequestSchema = new mongoose.Schema(
     {
             'from': { type: String, required: true, lowercase: true, trim: true },
-            'amount': { type: String, required: true,enum:['Org1','Org2']},
+            'amount': { type: String, required: true},
             'status':{ type: String , default:'pending'},
             'txId': { type: String}
 
